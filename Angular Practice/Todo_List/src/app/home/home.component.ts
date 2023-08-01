a31c182ff3113945b88dc3651c7c26bb3ac8e50e
Rifat <rifathossain328663@gmail.com>
2023-08-01 23:00:01 +0600

    Data binding or interpulation, pipe, Event binding

--- Angular Practice/Todo_List/src/app/home/home.component.ts
+++ Angular Practice/Todo_List/src/app/home/home.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  name= "Rifat Hossain"
+  salary=50000;
+  yearly=this.salary*12
 
+  clickFunction(){
+    alert(this.name +' \n Salary:'+ this.salary+ '\nyearly Salary:'+this.yearly);
+  }
+  clickFunctionA(name:string){
+    alert(name);
+  }
 }
