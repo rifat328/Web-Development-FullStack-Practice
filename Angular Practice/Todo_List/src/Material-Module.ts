88d78f04296b0c0ef62510f086887208a6b83b37
Rifat <rifathossain328663@gmail.com>
2023-08-03 22:29:56 +0600

    Log-in form start

--- Angular Practice/Todo_List/src/Material-Module.ts
+++ Angular Practice/Todo_List/src/Material-Module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from "@angular/core";
 import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatTableModule} from '@angular/material/table';
+import {MatButtonModule} from '@angular/material/button';
 @NgModule({
     exports:[
-        MatCardModule
+        MatCardModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatTableModule,
+        MatButtonModule
     ]
 })
 export class MaterialModule{
