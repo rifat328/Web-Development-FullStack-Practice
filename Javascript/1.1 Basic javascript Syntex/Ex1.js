534a9d460ae5e9eb04dfd2d47ac45ea353f55f8f
Rifat <rifathossain328663@gmail.com>
2024-09-24 11:08:53 +0600

    upuntil 81

---
+++ Javascript/1.1 Basic javascript Syntex/Ex1.js
@@ -0,0 +1,16 @@
+alert('hello'+' '+ 'World');
+window.alert("bangla vai");
+console.log(5+5);
+
+typeof(123);
+
+typeof(false); //Shows the datatype
+var myName="Rifat Hossain";
+typeof("Rifat Hossain");
+
+var name=prompt("what is Your Name");
+if( this.name === this.myName){
+    alert("Right User Detected");
+}else{
+    alert("Wrong User");
+}
---
+++ Javascript/1.1 Basic javascript Syntex/Ex2.js
@@ -0,0 +1,13 @@
+function test() {
+    var a="3";
+    var b="8";
+
+    // Swap value
+    var c=a;
+    a=b;
+    b=c;
+    console.log("a is "+a);
+    console.log("b is "+b);
+}
+
+test();
