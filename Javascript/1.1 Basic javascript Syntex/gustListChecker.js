371ff4b0aa18f59362e2c2d39cdb7d635e30fd4b
Rifat <rifathossain328663@gmail.com>
2024-10-02 14:20:47 +0600

    guestlist

---
+++ Javascript/1.1 Basic javascript Syntex/gustListChecker.js
@@ -0,0 +1,10 @@
+var userInput=prompt("Enter Your Name .."); 
+let guestList=["angela","Hau","rifat","hossain"];
+
+function guestListChacker(name){
+    if( guestList.includes(name) == true){
+        alert("You Are Invited :: Congratulation");
+    }else alert("!! Sorry Not Invited");
+}
+
+guestListChacker(userInput);
