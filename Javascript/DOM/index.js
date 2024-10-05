f3fd2464f05673acf3c22fcc0f433671a4085fea
Rifat <rifathossain328663@gmail.com>
2024-10-05 22:02:22 +0600

    DOM Test

---
+++ Javascript/DOM/index.js
@@ -0,0 +1,11 @@
+function banglaVai(){
+    let click=false;
+    if(click == false){
+        click=true;
+        document.querySelector("h1").innerHTML="Bangla Vai";
+    }else{
+        click=false;
+        document.querySelector("h1").innerHTML="Hello World";
+    }
+    
+}
