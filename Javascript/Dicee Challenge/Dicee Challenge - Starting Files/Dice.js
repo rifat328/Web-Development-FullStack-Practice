9408d9e8a9063272ac052b01341f268bb413a545
Rifat <rifathossain328663@gmail.com>
2024-10-12 13:03:34 +0600

    Initial

---
+++ Javascript/Dicee Challenge/Dicee Challenge - Starting Files/Dice.js
@@ -0,0 +1,41 @@
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+//   function refreshCheck()
+//           {
+//             if( document.refresh.visited.value == "" )
+//             {
+//               // Page loaded for first time
+//               document.refresh.visited.value = "1";
+//               alert("Fresh!!");
+//             }
+//             else
+//             {
+//               //On page refresh 
+//               alert("Refresh!!");
+//             }
+//           }
