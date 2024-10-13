e2b98cf30f3ed3d71d0d3b66b26ae29248b3853b
Rifat <rifathossain328663@gmail.com>
2024-10-13 15:38:56 +0600

    Modification

--- Javascript/Dicee Challenge/Dicee Challenge - Starting Files/Dice.js
+++ Javascript/Dicee Challenge/Dicee Challenge - Starting Files/Dice.js
@@ -9,23 +9,37 @@
     // Not the first visit, so alert
     // alert('You refreshed!');
     var number=randomNumberGenerator();
+    if(number[0]>number[1]){
+      //player 1 wins
+      document.getElementsByTagName("h1").textContent="🚩 Player 1 Wins";
+
+    }else{
+      //Player  2 Wins.
+            document.getElementsByTagName("h1").textContent="🚩 Player 2 Wins";
+      
+    }
   }
 }
   
  function randomNumberGenerator(){
     var number=[];
+    var img=[];
     var random_number=Math.floor(Math.random()*7)+1;
-     number.push("dice"+random_number+".png");
+     img.push("dice"+random_number+".png");
+     number.push(random_number);
     var random_number2=Math.floor(Math.random()*7)+1;
-    number.push("dice"+random_number2+".png");
-    return number;
+    img.push("dice"+random_number2+".png");
+    number.push(random_number2);
+    return number,img;
  } 
   
- var number=randomNumberGenerator();
+
   
   
   
-  
+  // arr position assign to player 1 , arr 2 position assign to player 2 
+  // if position 1 value > position 2 value then player 1 win 
+  // or else player 2 wins  
   
 //   function randomNumberGenerator() {
 //     var number = [];
