3ef10b39892f3a358a00c0f8b2ce1f5c63c57be6
Rifat <rifathossain328663@gmail.com>
2024-10-14 22:03:10 +0600

    This Object & Play Sound

--- Javascript/Advenced DOM Event Listener/Drum Kit Starting Files (1)/Drum Kit Starting Files/index.js
+++ Javascript/Advenced DOM Event Listener/Drum Kit Starting Files (1)/Drum Kit Starting Files/index.js
@@ -2,6 +2,11 @@ var arr=['w','a','s','d','j','k','l'];
 for(let i=0;i<=6;i++){
 
     document.querySelectorAll("button")[i].addEventListener("click",function(){
-        alert("i got clicked "+arr[i]);
+        this.style.color="white";
     })
 }
+
+
+
+// var audio= new Audio("sounds/tom-1.mp3");
+//             audio.play();
