787a2181714624bbbbdf82de579e86e8d057dbd7
Rifat <rifathossain328663@gmail.com>
2024-10-14 23:11:14 +0600

    Consturctor Function

---
+++ Javascript/Advenced DOM Event Listener/objectConstructor.js
@@ -0,0 +1,18 @@
+function HouseKeeper(name, workExperience, gender, workPermit,languages, workAffinity){
+    this.name=name;
+    this.workExperience=workExperience;
+    this.gender=gender;
+    this.workPermit=workPermit;
+    this.languages=languages;
+    this.workAffinity=workAffinity;
+}
+
+var housekeeper1= new HouseKeeper("Ranisa",40, "Female", "Yes", ["Bangla","English","Hindi","Uganda"], ["HouseKeeping","Cleaning","Bathroom Wasing"]);
+var housekeeper2= new HouseKeeper("Bulma",30, "Female", "Yes", ["Bangla","English","Hindi","Uganda"], ["HouseKeeping","Cleaning","Bathroom Wasing"]);
+housekeeper1.gender;
+housekeeper1.name;
+housekeeper1.workAffinity
+
+housekeeper2.name;
+housekeeper2.languages;
+housekeeper2.workAffinity;
