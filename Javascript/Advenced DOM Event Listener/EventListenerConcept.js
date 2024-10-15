7b44ab7e44f5bc3d5acfb777a176430f708a41e9
Rifat <rifathossain328663@gmail.com>
2024-10-15 15:22:24 +0600

    callBack added and fixed

--- Javascript/Advenced DOM Event Listener/EventListenerConcept.js
+++ Javascript/Advenced DOM Event Listener/EventListenerConcept.js
@@ -1,14 +1,21 @@
-function anotherEventListener(typeOffEvent,callBack){
-
-    //Detect Event Code.
-
-    var eventThatHappend={
-        eventType:"keypress",
+function anotherEventListener(typeOffEvent, callBack) {
+    // Detect Event Code.
+    var eventThatHappend = {
+        eventType: "keypress",
         key: "p",
-        duration:"2sec"
+        duration: "2sec"
     }
-
-    if (eventThatHappend == typeOffEvent){
-        callBack=eventThatHappend;
+    
+    if (eventThatHappend.eventType == typeOffEvent) {
+        callBack(eventThatHappend); // Call the callback with the event object
     }
 }
+
+anotherEventListener("keypress", function (event) {
+    console.log(event); // This should now log the event object to the console
+});
+
+
+anotherEventListener("keypress", function (event) {
+    console.log(event.key); // This should now log the event object to the console
+});
