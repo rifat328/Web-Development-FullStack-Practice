cb015f80acd61342a5a495be6478326c9748e62c
Rifat Hossain <69417553+rifat328@users.noreply.github.com>
2024-10-25 20:36:10 +0600

    Node Modules  for file system node internal built in Modules  find in node documentation

---
+++ Javascript/Node/nodeApisFileSystem.js
@@ -0,0 +1,5 @@
+const fileSystem= require("fs");
+fileSystem.copyFileSync("file1.txt", "text.txt"); // copy file 1 content to file 2 ;
+// these are node internal built in Moduls ,
+// there are also external Modules like packeges that we can use using npm 
+// these are written by other people 
