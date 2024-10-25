02662be3bf68208bfaa4de38a489676b30c7477d
Rifat Hossain <69417553+rifat328@users.noreply.github.com>
2024-10-25 21:28:09 +0600

    external packege use

--- Javascript/Node/index.js
+++ Javascript/Node/index.js
@@ -4,3 +4,9 @@ console.log("Hello world with Node");
 // REPL read evaluation print loop is like browser console
 // to enter type node 
 // to exit type .exit or ctrl + c twice
+
+import superheroes  from 'superheroes';
+import {randomSuperhero} from 'superheroes';
+let superheoname=randomSuperhero();
+
+console.log(superheoname);
