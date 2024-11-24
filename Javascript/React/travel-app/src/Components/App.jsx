431ca0a90ec0414f8fac663c237ef96c652998c6
Rifat Hossain <69417553+rifat328@users.noreply.github.com>
2024-11-24 22:35:23 +0600

    Contacts prop added

--- Javascript/React/travel-app/src/Components/App.jsx
+++ Javascript/React/travel-app/src/Components/App.jsx
@@ -8,7 +8,7 @@ const App = () => {
   return (
     <div>
       <Header/>
-      <Contacts/>
+      
     </div>
   )
 }
--- Javascript/React/travel-app/src/Components/Contacts.jsx
+++ Javascript/React/travel-app/src/Components/Contacts.jsx
@@ -1,97 +1,28 @@
 import React from 'react'
 import '../Styles/contacts.css'
-const Contacts = () => {
+const Contacts = ({img,name,alt,mail,phone}) => {
   return (
-    <div className="contacts">
-            <article className="contact-card">
-                <img 
-                    src="https://picsum.photos/200"
-                    alt="Photo of Mr. Whiskerson"
+    <article className="contact-card">
+            <img
+                src={img}
+                alt={alt}
+            />
+            <h3>{name}</h3>
+            <div className="info-group">
+                <img
+                    src="./images/phone-icon.png"
+                    alt="phone icon"
                 />
-                <h3>Mr. Whiskerson</h3>
-                <div className="info-group">
-                    <img 
-                        src="" 
-                        alt="phone icon" 
-                    />
-                    <p>(212) 555-1234</p>
-                </div>
-                <div className="info-group">
-                    <img 
-                        src="./images/mail-icon.png" 
-                        alt="mail icon"
-                    />
-                    <p>mr.whiskaz@catnap.meow</p>
-                </div>
-            </article>
-            
-            <article className="contact-card">
-                <img 
-                    src="https://picsum.photos/200"
-                    alt="Photo of Fluffykins"
+                <p>{phone}</p>
+            </div>
+            <div className="info-group">
+                <img
+                    src="./images/mail-icon.png"
+                    alt="mail icon"
                 />
-                <h3>Fluffykins</h3>
-                <div className="info-group">
-                    <img 
-                        src="./images/phone-icon.png" 
-                        alt="phone icon" 
-                    />
-                    <p>(212) 555-2345</p>
-                </div>
-                <div className="info-group">
-                    <img 
-                        src="./images/mail-icon.png" 
-                        alt="mail icon"
-                    />
-                    <p>fluff@me.com</p>
-                </div>
-            </article>
-            
-            <article className="contact-card">
-                <img 
-                    src="https://picsum.photos/200"
-                    alt="Photo of Felix"
-                />
-                <h3>Felix</h3>
-                <div className="info-group">
-                    <img 
-                        src="./images/phone-icon.png" 
-                        alt="phone icon" 
-                    />
-                    <p>(212) 555-4567</p>
-                </div>
-                <div className="info-group">
-                    <img 
-                        src="./images/mail-icon.png" 
-                        alt="mail icon"
-                    />
-                    <p>thecat@hotmail.com</p>
-                </div>
-            </article>
-            
-            <article className="contact-card">
-                <img 
-                    src="https://picsum.photos/200"
-                    alt="Photo of Pumpkin"
-                />
-                <h3>Pumpkin</h3>
-                <div className="info-group">
-                    <img 
-                        src="./images/phone-icon.png" 
-                        alt="phone icon" 
-                    />
-                    <p>(0800) CAT KING</p>
-                </div>
-                <div className="info-group">
-                    <img 
-                        src="./images/mail-icon.png" 
-                        alt="mail icon"
-                    />
-                    <p>pumpkin@scrimba.com</p>
-                </div>
-            </article>
-            
-        </div>
+                <p>{mail}</p>
+            </div>
+        </article>
   )
 }
 
