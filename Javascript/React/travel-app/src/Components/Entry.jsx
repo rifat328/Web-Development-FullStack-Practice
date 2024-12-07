4f7541e6697f289c73aabbbcbbd3cfa8945f0402
Rifat Hossain <69417553+rifat328@users.noreply.github.com>
2024-12-07 22:31:40 +0600

    Entry Component and css changes

--- Javascript/React/travel-app/src/Components/Entry.jsx
+++ Javascript/React/travel-app/src/Components/Entry.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
-
+import { FaMapPin } from "react-icons/fa6";
 const Entry = (props) => {
   return (
     <div>
-        
+        <article className="journal-entry">
+            <div className="main-image-container">
+                <img 
+                    className="main-image"
+                    src={props.img.src} 
+                    alt={props.img.alt} 
+                />
+            </div>
+            <div className="info-container">
+                <FaMapPin/>
+                <span className="country"><strong>{props.country}</strong> </span>
+                <a href={props.googleMapLink}>View on Google Maps</a>
+                <h2 className="entry-title">{props.title}</h2>
+                <p className="trip-dates">{props.date}</p>
+                <p className="entry-text">{props.locationInfo}</p>
+            </div>
+            
+        </article>
     </div>
   )
 }
