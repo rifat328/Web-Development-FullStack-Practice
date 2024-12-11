dd52d13ad453fcc6e18b34052ba90ed6a55e17c9
Rifat Hossain <69417553+rifat328@users.noreply.github.com>
2024-12-11 16:03:59 +0600

    array map conversions

--- Javascript/1.1 Basic javascript Syntex/array.js
+++ Javascript/1.1 Basic javascript Syntex/array.js
@@ -18,3 +18,21 @@ console.log(animels);
 console.log(animels.at(2));
 console.log("______---_______---__________--____");
 console.log(animels.copyWithin(0, 2 ,4));
+//! array map
+const capitalized = names.map(name => name.charAt(0).toUpperCase() + name.slice(1));
+
+console.log(capitalized); // ["Alice", "Bob", "Charlie", "Danielle"]
+
+const nums = [1, 2, 3, 4, 5]
+// -->       [1, 4, 9, 16, 25]
+// Your code here
+const squares = nums.map(function(num) {
+    return num * num
+})
+
+
+const pokemon = ["Bulbasaur", "Charmander", "Squirtle"]
+// -->          ["<p>Bulbasaur</p>", "<p>Charmander</p>", "<p>Squirtle</p>"]
+// Your code here
+
+console.log(pokemon.map((pok)=>'<p>'+pok+'</p>'));
