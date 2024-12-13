ef9148e78e807550b1f7036476bc3d735b98e55b
Rifat Hossain <69417553+rifat328@users.noreply.github.com>
2024-12-13 21:02:04 +0600

    logical array rendering

--- Javascript/React/travel-app/src/Components/App.jsx
+++ Javascript/React/travel-app/src/Components/App.jsx
@@ -1,29 +1,28 @@
-import { useState } from 'react'
-import React from 'react'
-import '../App.css'
-import Header from './Header'
-import Entry from './Entry'
+import { useState } from "react";
+import React from "react";
+import "../App.css";
+import Header from "./Header";
+import Entry from "./Entry";
+import data from "../data.js";
 
+const journalEntry = data.map((journal) => (
+  <Entry
+    id={journal.id}
+    img={journal.img}
+    title={journal.title}
+    country={journal.country}
+    googleMapsLink={journal.googleMapsLink}
+    dates={journal.dates}
+    text={journal.text}
+  />
+));
 const App = () => {
   return (
     <div>
-      <Header/>
-      <main className='container'>
-          <Entry
-              img={{src:'https://scrimba.com/links/travel-journal-japan-image-url',
-              alt:'Mount Fuji'}}
-              country='Japan'
-              googleMapLink='https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu'
-              date='12 Jan, 2021 - 24 Jan, 2021'
-              title='Mount Fuji'
-              locationInfo='Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet).
-               Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.'
-            />
-      </main>
-      
-      
+      <Header />
+      <main className="container">{journalEntry}</main>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ Javascript/React/travel-app/src/data.js
@@ -0,0 +1,38 @@
+export default [
+  {
+    id: 1,
+    img: {
+      src: "https://scrimba.com/links/travel-journal-japan-image-url",
+      alt: "Mount Fuji",
+    },
+    title: "Mount Fuji",
+    country: "Japan",
+    googleMapsLink: "https://maps.app.goo.gl/6RLYZDuuuqJ7kNGZ9",
+    dates: "12 Jan, 2021 - 24 Jan, 2021",
+    text: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+  },
+  {
+    id: 2,
+    img: {
+      src: "https://scrimba.com/links/travel-journal-australia-image-url",
+      alt: "Sydney Opera House",
+    },
+    title: "Sydney Opera House",
+    country: "Australia",
+    googleMapsLink: "https://maps.app.goo.gl/Zr17SCrsJeCEKMd36",
+    dates: "27 May, 2021 - 8 Jun, 2021",
+    text: "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings.",
+  },
+  {
+    id: 3,
+    img: {
+      src: "https://scrimba.com/links/travel-journal-norway-image-url",
+      alt: "Geirangerfjord",
+    },
+    title: "Geirangerfjord",
+    country: "Norway",
+    googleMapsLink: "https://maps.app.goo.gl/fhkJuBhmFDv47tiB7",
+    dates: "01 Oct, 2021 - 18 Nov, 2021",
+    text: "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.",
+  },
+];
