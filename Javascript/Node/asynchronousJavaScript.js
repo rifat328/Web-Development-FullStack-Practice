3b145cfb75694649293e382bcd351fab72a4b900
Rifat Hossain <69417553+rifat328@users.noreply.github.com>
2024-12-25 22:40:07 +0600

    Asynchronous setTimeOut and SetInterval

---
+++ Javascript/Node/asynchronousJavaScript.js
@@ -0,0 +1,22 @@
+setTimeout(myFunction, 3000);
+function myFunction(value) {
+  console.log(`CallBack Function is called after ${3} Seconds`);
+  value === undefined ? (value = null) : console.log(value);
+}
+
+setTimeout(() => {
+  const value = "Bangladesh";
+  myFunction(value);
+}, 1000);
+
+const intervalId = setInterval(() => {
+  let date = new Date();
+  console.log(
+    date.getSeconds() + ":" + date.getMinutes() + ":" + date.getHours()
+  );
+}, 1000);
+
+setTimeout(() => {
+  clearInterval(intervalId);
+  console.log("Intervel Cleared.");
+}, 10000);
