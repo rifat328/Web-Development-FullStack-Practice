150776588a90cc83c5fbb243c02a7172a4a14898
Rifat Hossain <69417553+rifat328@users.noreply.github.com>
2024-12-27 23:14:16 +0600

    userEffect Quiz

--- Javascript/React/UseEffect.jsx
+++ Javascript/React/UseEffect.jsx
@@ -15,7 +15,36 @@ const UseEffect = () => {
   /*  react compare dependency array value with previous value of that array 
   if that changes then react will run that callBack function inside of the useEffact
   unless it won't this helps us to make api calls safely without infinite api call bec without this react will call api 
-  every time it hed to renderd */
+  every time it hed to renderd 
+  
+  Quiz--
+    1. In what way are React components meant to be "pure functions"
+- Given the same props or state, the component will always return
+  the same content, or UI
+- Rendering and re-rendering a component will never have any kind
+  of side effect on an outside system
+
+  2. What is a "side effect" in React? What are some examples?
+- Any code that affects or interacts with an outside system
+- local storage, API, websockets, DOM manipulation
+
+    3. What is NOT a "side effect" in React? Examples?
+- Anything that React is in charge of.
+- Maintaining state, keeping the UI in sync with data, 
+  rendering DOM elements
+
+  4. When does React run your useEffect function? When does it NOT run
+   the effect function?
+- As soon as the component renders for the first time
+- On every re-render of the component (assuming no dependencies array)
+- Will NOT run the effect when the values of the dependencies in the
+  array stay the same between renders
+
+  5. How would you explain what the "dependecies array" is?
+- Second parameter to the useEffect function
+- A way for React to know whether or not it should re-run the effect function
+  
+  */
   return (
     <div>
       <h2>The count is {count}</h2>
