d1da7ac04602cc4d8f19d6d6cd5232b5b641b793
Rifat Hossain <69417553+rifat328@users.noreply.github.com>
2025-01-20 04:53:07 +0600

    express simple calculator , File send to client , form , tu=> 208 complete

--- Express/calculator/calculator.js
+++ Express/calculator/calculator.js
@@ -2,29 +2,13 @@ const express = require("express");
 const app = express();
 const port = 4000;
 
-// '/' root home directory.
 app.get("/", (req, res) => {
-  res.send("<h1>Hello World</h1>");
-  //   console.log(req);
-});
-
-app.get("/contact", (req, res) => {
-  res.send("<h2>Contact me rifathossain328663@gmail.com</h2>");
-  //   console.log(req);
-});
-
-app.get("/about", (req, res) => {
-  res.send(
-    `<h2> I am a Software Engineer  need Any Help ?  contact me at @rifathossain328 </h2>`
-  );
+  console.log(__dirname);
+  res.sendFile(__dirname + "/index.html");
 });
 app.listen(port, () => {
-  console.log(`Example App Listening on Port ${port}`);
+  console.log(`Listening on port : ${port}`);
 });
-
-const add = (num1, num2) => {
-  return num1 + num2;
-};
 const subtract = (num1, num2) => num1 - num2;
 const multiplication = (num1, num2) => num1 * num2;
 const devide = (num1, num2) => num1 / num2;
